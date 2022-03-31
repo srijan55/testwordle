bc7703eb9b55b2b11613eff331c4399cd5502256
kavishavi <30476789+kavishavi@users.noreply.github.com>
2022-03-30 18:03:59 -0700

    Update main.3d28ac0c.js
    changed line:      l = "Wordle ".concat(s);
    to      l = "Hinglish Wordle ".concat(s);

--- main.3d28ac0c.js
+++ main.3d28ac0c.js
@@ -2441,7 +2441,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle =  function(e) {
                                     o = e.isWin,
                                     r = JSON.parse(window.localStorage.getItem(j)),
                                     i = JSON.parse(window.localStorage.getItem(S)),
-                                    l = "Wordle ".concat(s);
+                                    l = "Hinglish Wordle ".concat(s);
                                 l += " ".concat(o ? t : "X", "/").concat(6), n && (l += "*");
                                 var d = "";
                                 return a.forEach((function(e) {
